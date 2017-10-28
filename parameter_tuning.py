2e836ee5d47524a4b12514100167ebc0eb0953f9
LeonHardt <leonhardt427@126.com>
2017-10-28 20:46:15 +0800

    Save the parameter
    Save the parameter to txt

--- parameter_tuning.py
+++ parameter_tuning.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-from sklearn.linear_model import Perceptron
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -23,23 +22,22 @@ y = np.loadtxt('y_label.csv', delimiter=',', dtype='int8')
 """
 choose the classification method:
 Methods:
-    1. Perceptron       ------------------------         'ppn'
-    2. Logistic Regression -------------------------     'lr'
-    3. Support Vector Machine (linear kernel)----------  'svm'
-    4. Support Vector Machine (rbf kernel)-----------    'svm
-    5. Decision Tree        -------------------          'tree'
-    6. Random Forest    ------------------------         'forest'
+    1. Logistic Regression -------------------------     'lr'
+    2. Support Vector Machine -------------------------  'svm'
+    3. Decision Tree     ---------------------------     'tree'
+    4. Random Forest    ------------------------         'forest'
 """
 # Set the parameter of the classification method
-METHOD = 'FOREST'
+METHOD = 'ALL'
 REPORT_NEED = 0
 param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
-max_depth_range = [4, 8, 12, 16, 20]
+param_range = [0.1, 1.0, 10.0]
+max_depth_range = [4, 8, 12,]
 
 """
 if method is 'LR':
 """
-if METHOD is 'LR':
+if METHOD is 'LR' or METHOD is 'ALL':
     pip_lr = Pipeline([('scl', StandardScaler()), ('clf', LogisticRegression(random_state=1))])
 
     param_grid = [{'clf__C': param_range}]
@@ -53,6 +51,12 @@ if METHOD is 'LR':
             print("%0.3f (+/-%0.03f) for %r" % (gs.cv_results_['mean_test_score'][number],
                                                 gs.cv_results_['std_test_score'][number],
                                                 gs.cv_results_['params'][number],))
+
+    with open("./parameter_result/lr_parameter.txt", "w") as lr:
+        lr.write('mean_test_score: ' + str(gs.cv_results_['mean_test_score']) + '\n' + 'std_test_score: ' +
+                 str(gs.cv_results_['std_test_score']) + '\nparameters: ' + str(gs.cv_results_['params']) + '\n' +
+                 'Best parameters of Logistic Regression is: ' + str(gs.best_params_) + '\n' +
+                 'Best score of Logistic Regression is: ' + str(gs.best_score_))
     print("Best parameters of Logistic Regression is:")
     print(gs.best_params_)
     print(gs.best_score_)
@@ -60,20 +64,25 @@ if METHOD is 'LR':
 """
 if method is 'SVM':
 """
-if METHOD is 'SVM':
+if METHOD is 'SVM' or METHOD is 'ALL':
     pip_svm = Pipeline([('scl', StandardScaler()), ('clf', SVC(random_state=1))])
     param_grid = [{'clf__C': param_range, 'clf__kernel': ['linear']},
                   {'clf__C': param_range, 'clf__gamma': param_range, 'clf__kernel': ['rbf']}]
     gs = GridSearchCV(estimator=pip_svm, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
     gs = gs.fit(X, y)
 
-# Show the full report (Yes: 1 ; No: 0)
-#     if REPORT_NEED is 1:
-#         print()
-#         for number in range(len(param_range)):
-#             print("%0.3f (+/-%0.03f) for %r" % (gs.cv_results_['mean_test_score'][number],
-#                                                 gs.cv_results_['std_test_score'][number],
-#                                                 gs.cv_results_['params'][number],))
+    # Show the full report (Yes: 1 ; No: 0)
+    if REPORT_NEED is 1:
+        print()
+        for number in range(len(param_range)):
+            print("%0.3f (+/-%0.03f) for %r" % (gs.cv_results_['mean_test_score'][number],
+                                                gs.cv_results_['std_test_score'][number],
+                                                gs.cv_results_['params'][number],))
+    with open("./parameter_result/SVM_parameter.txt", "w") as svm_parameter:
+        svm_parameter.write('mean_test_score: ' + str(gs.cv_results_['mean_test_score']) + '\n' + 'std_test_score: ' +
+                            str(gs.cv_results_['std_test_score']) + '\nparameters: ' + str(gs.cv_results_['params']) + '\n' +
+                            'Best parameters of SVM is: ' + str(gs.best_params_) + '\n' +
+                            'Best score of SVM is: ' + str(gs.best_score_))
     print("Best parameters of SVM is:")
     print(gs.best_params_)
     print(gs.best_score_)
@@ -82,7 +91,7 @@ if METHOD is 'SVM':
 """
 if method is 'tree':
 """
-if METHOD is 'TREE':
+if METHOD is 'TREE' or METHOD is 'ALL':
     pip_tree = Pipeline([('scl', StandardScaler()), ('clf', DecisionTreeClassifier(random_state=1))])
     param_grid = [{'clf__criterion': ['entropy'], 'clf__max_depth': max_depth_range}]
     gs = GridSearchCV(estimator=pip_tree, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
@@ -95,6 +104,11 @@ if METHOD is 'TREE':
             print("%0.3f (+/-%0.03f) for %r" % (gs.cv_results_['mean_test_score'][number],
                                                 gs.cv_results_['std_test_score'][number],
                                                 gs.cv_results_['params'][number],))
+    with open("./parameter_result/Tree_parameter.txt", "w") as tree_parameter:
+        tree_parameter.write('mean_test_score: ' + str(gs.cv_results_['mean_test_score']) + '\n' + 'std_test_score: ' +
+                             str(gs.cv_results_['std_test_score']) + '\nparameters: ' + str(gs.cv_results_['params']) +
+                             '\n' + 'Best parameters of Tree is: ' + str(gs.best_params_) + '\n' +
+                             'Best score of Tree is: ' + str(gs.best_score_))
     print("Best parameters of Tree is:")
     print(gs.best_params_)
     print(gs.best_score_)
@@ -102,7 +116,7 @@ if METHOD is 'TREE':
 """
 if method is 'forest':
 """
-if METHOD is 'FOREST':
+if METHOD is 'FOREST' or METHOD is 'ALL':
     pip_forest = Pipeline([('scl', StandardScaler()), ('clf', RandomForestClassifier(random_state=1))])
     param_grid = [{'clf__criterion': ['entropy'], 'clf__max_depth': max_depth_range}]
     gs = GridSearchCV(estimator=pip_forest, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
@@ -115,6 +129,11 @@ if METHOD is 'FOREST':
             print("%0.3f (+/-%0.03f) for %r" % (gs.cv_results_['mean_test_score'][number],
                                                 gs.cv_results_['std_test_score'][number],
                                                 gs.cv_results_['params'][number],))
+    with open("./parameter_result/forest_parameter.txt", "w") as forest_parameter:
+        forest_parameter.write('mean_test_score: ' + str(gs.cv_results_['mean_test_score']) + '\n' + 'std_test_score: ' +
+                               str(gs.cv_results_['std_test_score']) + '\nparameters: ' + str(gs.cv_results_['params']) +
+                               '\n' + 'Best parameters of Forest is: ' + str(gs.best_params_) + '\n' +
+                               'Best score of Forest is: ' + str(gs.best_score_))
     print("Best parameters of FOREST is:")
     print(gs.best_params_)
     print(gs.best_score_)
