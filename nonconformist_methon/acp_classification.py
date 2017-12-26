627424a8aee71aad5db271aac609f1be16cdeff2
LeonHardt427 <leonhardt427@126.com>
2017-12-26 18:42:33 +0800

    acp_conformal_prediction

--- nonconformist_methon/acp_classification.py
+++ nonconformist_methon/acp_classification.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split , StratifiedKFold
 
 from nonconformist.base import ClassifierAdapter
 from nonconformist.nc import ClassifierNc
@@ -19,7 +19,7 @@ from nonconformist.acp import AggregatedCp
 from nonconformist.acp import BootstrapSampler, CrossSampler, RandomSubSampler
 from nonconformist.acp import BootstrapConformalClassifier
 from nonconformist.acp import CrossConformalClassifier
-from nonconformist.evaluation import class_mean_errors, class_avg_c
+from nonconformist.evaluation import class_mean_errors, class_avg_c, class_empty
 
 # -----------------------------------------------------------------------------
 # Experiment setup
@@ -33,17 +33,16 @@ sc = StandardScaler()
 
 X = sc.fit_transform(X)
 
-# idx = np.random.permutation(y.size)
-# train = idx[:int(2 * idx.size / 3)]
-# test = idx[int(2 * idx.size / 3):]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-truth = y_test.reshape(-1, 1)
 columns = ['C-{}'.format(i) for i in np.unique(y)] + ['truth']
 significance = 0.45
 classification_method = DecisionTreeClassifier()
+file_name = 'decision_tree.xls'
 
+ACP_Random = []
+ACP_Cross = []
+ACP_Boot = []
+CCP = []
+BCP = []
 # -----------------------------------------------------------------------------
 # Define models
 # -----------------------------------------------------------------------------
@@ -73,15 +72,92 @@ models = {  'ACP-RandomSubSampler'  : AggregatedCp(
                                                 ClassifierAdapter(classification_method))))
           }
 
-# -----------------------------------------------------------------------------
-# Train, predict and evaluate
-# -----------------------------------------------------------------------------
-for name, model in models.items():
-    model.fit(X_train, y_train)
-    prediction = model.predict(X_test, significance=None)
-    table = np.hstack((prediction, truth))
-    df = pd.DataFrame(table, columns=columns)
-    print('\n{}'.format(name))
-    print('Error rate: {}'.format(class_mean_errors(prediction, truth, significance)))
-    print('class average count: {}'.format(class_avg_c(prediction, truth, significance)))
-    # print(df)
+# ------------------------------------------------------------------------------------------------------------------
+# Train_test_split method:
+# ------------------------------------------------------------------------------------------------------------------
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#
+# truth = y_test.reshape(-1, 1)
+# columns = ['C-{}'.format(i) for i in np.unique(y)] + ['truth']
+# significance = 0.45
+# classification_method = DecisionTreeClassifier()
+#
+#
+# for name, model in models.items():
+#     model.fit(X_train, y_train)
+#     prediction = model.predict(X_test, significance=None)
+#     table = np.hstack((prediction, truth))
+#     df = pd.DataFrame(table, columns=columns)
+#     print('\n{}'.format(name))
+#     print('Error rate: {}'.format(class_mean_errors(prediction, truth, significance)))
+#     print('class average count: {}'.format(class_avg_c(prediction, truth, significance)))
+#     # print(df)
+#
+
+
+# ------------------------------------------------------------------------------------------------------------------
+# StratifiedKFold method
+# ------------------------------------------------------------------------------------------------------------------
+
+s_folder = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+summary = []
+for train_index, test_index in s_folder.split(X, y):
+    x_train, x_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    truth = y_test.reshape(-1, 1)
+
+    for name, model in models.items():
+        result = []
+        model.fit(x_train, y_train)
+        prediction = model.predict(x_test, significance=None)
+        table = np.hstack((prediction, truth))
+        df = pd.DataFrame(table, columns=columns)
+        print('\n{}'.format(name))
+        print('Error rate: {}'.format(class_mean_errors(prediction, truth, significance)))
+        print('class average count: {}'.format(class_avg_c(prediction, truth, significance)))
+        result = [name,
+                  class_mean_errors(prediction, truth, significance),
+                  class_avg_c(prediction, truth, significance),
+                  class_empty(prediction, truth, significance)]
+
+        if len(summary) == 0:
+            summary = result
+        else:
+            summary = np.vstack((summary, result))
+
+
+        # if name == 'ACP-RandomSubSampler':
+        #     if index == 0:
+        #         ACP_Random = result
+        #     else:
+        #         ACP_Random = np. vstack((ACP_Random, result))
+        # elif name == 'ACP-CrossSampler':
+        #     if index == 0:
+        #         ACP_Cross = result
+        #     else:
+        #         ACP_Cross = np. vstack((ACP_Cross, result))
+        # elif name == 'ACP-BootstrapSampler':
+        #     if index == 0:
+        #         ACP_Boot = result
+        #     else:
+        #         ACP_Boot = np. vstack((ACP_Boot, result))
+        # elif name == 'CCP':
+        #     if index == 0:
+        #         CCP = result
+        #     else:
+        #         CCP = np.vstack((CCP, result))
+        # elif name == 'BCP':
+        #     if index == 0:
+        #         BCP = result
+        #     else:
+        #         BCP = np.vstack((BCP, result))
+
+df_result = pd.DataFrame(summary, columns=['name', 'class_mean_errors', 'class_avg_c', 'class_empty'])
+df_result.to_excel(file_name, sheet_name='Sheet1')
+
+
+
+
+
+
