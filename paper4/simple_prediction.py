d17a2a77e238b39ebb10c2cc93c4ba423e674c82
LeonHardt <leonhardt427@126.com>
2018-02-22 21:36:09 +0800

    simple_prediction

---
+++ paper4/simple_prediction.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/22 下午6:05
+# @Author  : LeonHardt
+# @File    : simple_prediction.py
+
+"""
+Simple prediciton in paper 4
+"""
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import StratifiedKFold
+
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
+# -----------------------------
+# preprocessing
+# -----------------------------
+
+X = np.loadtxt('x_sample.csv', delimiter=',')
+y = np.loadtxt('y_label.csv', delimiter=',')
+
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# ------------------------------
+# prediction
+# ------------------------------
+
+s_folder = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
+RF_result = []
+SVM_result = []
+for index, (train, test) in enumerate(s_folder.split(X, y)):
+    print('the {} time'.format(index))
+    x_train, x_test = X[train], X[test]
+    y_train, y_test = y[train], y[test]
+
+    lda = LinearDiscriminantAnalysis(n_components=5)
+    x_train_lda = lda.fit_transform(x_train, y_train)
+    x_test_lda = lda.transform(x_test)
+
+    rf = RandomForestClassifier(n_estimators=50, criterion='entropy')
+    svm = SVC(C=6000.0, gamma=0.001, probability=True)
+
+    rf.fit(x_train_lda, y_train)
+    svm.fit(x_train_lda, y_train)
+
+    rf_prediction = rf.predict(x_test_lda)
+    svm_prediciton = svm.predict(x_test_lda)
+
+    rf_accuracy = accuracy_score(y_test, rf_prediction)
+    svm_accuracy = accuracy_score(y_test, svm_prediciton)
+
+    print("RF is {}".format(rf_accuracy))
+    print("SVM is {}".format(svm_accuracy))
+
+    RF_result.append(rf_accuracy)
+    SVM_result.append(svm_accuracy)
+
+RF_acc = np.mean(RF_result)
+SVM_acc = np.mean(SVM_result)
+
+print("RF_mean is {}".format(RF_acc))
+print("SVM_mean is {}".format(SVM_acc))
+
+
+
--- supervised_learning/offline_Kfold.py
+++ supervised_learning/offline_Kfold.py
@@ -38,50 +38,50 @@ y = np.loadtxt('y_label.csv', delimiter=',', dtype='int8')
 K_fold = StratifiedKFold(n_splits=10, random_state=1)
 
 
-"""
-Method 1: Perceptron
-"""
-pipe_ppn = Pipeline([('scl', StandardScaler()), ('ppn', Perceptron(n_iter=100, eta0=0.1))])
-ppn_score = []
-fold_times = 1
-for train_index, test_index in K_fold.split(X, y):
-    pipe_ppn.fit(X[train_index], y[train_index])
-    score = pipe_ppn.score(X[test_index], y[test_index])
-    fold_times += 1
-    ppn_score.append(score)
-    # print('Perceptron: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
-ppn_score_cv = [np.mean(ppn_score), np.std(ppn_score)]
-print('Perceptron CV accuracy: %s +/- %s' % (ppn_score_cv[0], ppn_score_cv[1]))
+# """
+# Method 1: Perceptron
+# """
+# pipe_ppn = Pipeline([('scl', StandardScaler()), ('ppn', Perceptron(n_iter=100, eta0=0.1))])
+# ppn_score = []
+# fold_times = 1
+# for train_index, test_index in K_fold.split(X, y):
+#     pipe_ppn.fit(X[train_index], y[train_index])
+#     score = pipe_ppn.score(X[test_index], y[test_index])
+#     fold_times += 1
+#     ppn_score.append(score)
+#     # print('Perceptron: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
+# ppn_score_cv = [np.mean(ppn_score), np.std(ppn_score)]
+# print('Perceptron CV accuracy: %s +/- %s' % (ppn_score_cv[0], ppn_score_cv[1]))
 
-"""
-Method 2: Logistic Regression
-"""
-pipe_lr = Pipeline([('scl', StandardScaler()), ('lr', LogisticRegression(C=1000.0))])
-lr_score = []
-fold_times = 1
-for train_index, test_index in K_fold.split(X, y):
-    pipe_lr.fit(X[train_index], y[train_index])
-    score = pipe_lr.score(X[test_index], y[test_index])
-    fold_times += 1
-    lr_score.append(score)
-    # print('LOGISTIC: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
-lr_score_cv = [np.mean(lr_score), np.std(lr_score)]
-print('LOGISTIC CV accuracy: %s +/- %s' % (lr_score_cv[0], lr_score_cv[1]))
+# """
+# Method 2: Logistic Regression
+# """
+# pipe_lr = Pipeline([('scl', StandardScaler()), ('lr', LogisticRegression(C=1000.0))])
+# lr_score = []
+# fold_times = 1
+# for train_index, test_index in K_fold.split(X, y):
+#     pipe_lr.fit(X[train_index], y[train_index])
+#     score = pipe_lr.score(X[test_index], y[test_index])
+#     fold_times += 1
+#     lr_score.append(score)
+#     # print('LOGISTIC: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
+# lr_score_cv = [np.mean(lr_score), np.std(lr_score)]
+# print('LOGISTIC CV accuracy: %s +/- %s' % (lr_score_cv[0], lr_score_cv[1]))
 
-"""
-Method 3: Support Vector machine (linear kernel)
-"""
-pipe_svm_linear = Pipeline([('scl', StandardScaler()), ('svm_linear', SVC(kernel='linear', C=1.0))])
-svm_linear_score = []
-fold_times = 1
-for train_index, test_index in K_fold.split(X, y):
-    pipe_svm_linear.fit(X[train_index], y[train_index])
-    score = pipe_svm_linear.score(X[test_index], y[test_index])
-    fold_times += 1
-    svm_linear_score.append(score)
-    # print('SVM_LINEAR: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
-svm_linear_score_cv = [np.mean(svm_linear_score), np.std(svm_linear_score)]
-print('SVM_LINEAR CV accuracy: %s +/- %s' % (svm_linear_score_cv[0], svm_linear_score_cv[1]))
+# """
+# Method 3: Support Vector machine (linear kernel)
+# """
+# pipe_svm_linear = Pipeline([('scl', StandardScaler()), ('svm_linear', SVC(kernel='linear', C=1.0))])
+# svm_linear_score = []
+# fold_times = 1
+# for train_index, test_index in K_fold.split(X, y):
+#     pipe_svm_linear.fit(X[train_index], y[train_index])
+#     score = pipe_svm_linear.score(X[test_index], y[test_index])
+#     fold_times += 1
+#     svm_linear_score.append(score)
+#     # print('SVM_LINEAR: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
+# svm_linear_score_cv = [np.mean(svm_linear_score), np.std(svm_linear_score)]
+# print('SVM_LINEAR CV accuracy: %s +/- %s' % (svm_linear_score_cv[0], svm_linear_score_cv[1]))
 
 """
 Method 4: Support Vector machine (rbf kernel)
@@ -98,20 +98,20 @@ for train_index, test_index in K_fold.split(X, y):
 svm_rbf_score_cv = [np.mean(svm_rbf_score), np.std(svm_rbf_score)]
 print('SVM_RBF CV accuracy: %s +/- %s' % (svm_rbf_score_cv[0], svm_rbf_score_cv[1]))
 
-"""
-Method 5: Decision Tree 
-"""
-pipe_tree = Pipeline([('scl', StandardScaler()), ('tree', DecisionTreeClassifier(criterion='entropy', max_depth=8))])
-tree_score = []
-fold_times = 1
-for train_index, test_index in K_fold.split(X, y):
-    pipe_tree.fit(X[train_index], y[train_index])
-    score = pipe_tree.score(X[test_index], y[test_index])
-    fold_times += 1
-    tree_score.append(score)
-    # print('Tree: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
-tree_score_cv = [np.mean(tree_score), np.std(tree_score)]
-print('Tree CV accuracy: %s +/- %s' % (tree_score_cv[0], tree_score_cv[1]))
+# """
+# Method 5: Decision Tree
+# """
+# pipe_tree = Pipeline([('scl', StandardScaler()), ('tree', DecisionTreeClassifier(criterion='entropy', max_depth=8))])
+# tree_score = []
+# fold_times = 1
+# for train_index, test_index in K_fold.split(X, y):
+#     pipe_tree.fit(X[train_index], y[train_index])
+#     score = pipe_tree.score(X[test_index], y[test_index])
+#     fold_times += 1
+#     tree_score.append(score)
+#     # print('Tree: Fold: %s, Class dist.: %s, Acc: %.3f' % (fold_times, np.bincount(y[train_index]), score))
+# tree_score_cv = [np.mean(tree_score), np.std(tree_score)]
+# print('Tree CV accuracy: %s +/- %s' % (tree_score_cv[0], tree_score_cv[1]))
 
 """
 Method 6: Random Forest
@@ -128,19 +128,19 @@ for train_index, test_index in K_fold.split(X, y):
 forest_score_cv = [np.mean(forest_score), np.std(forest_score)]
 print('Forest CV accuracy: %s +/- %s' % (forest_score_cv[0], forest_score_cv[1]))
 
-"""
-Method 7: KNN
-"""
-pipe_knn = Pipeline([('scl', StandardScaler()), ('knn', KNeighborsClassifier(n_neighbors=1))])
-knn_score = []
-fold_times = 1
-for train_index, test_index in K_fold.split(X, y):
-    pipe_knn.fit(X[train_index], y[train_index])
-    score = pipe_knn.score(X[test_index], y[test_index])
-    fold_times += 1
-    knn_score.append(score)
-knn_score_cv = [np.mean(knn_score), np.std(knn_score)]
-print('KNN CV accuracy: %s +/- %s' % (knn_score_cv[0], knn_score_cv[1]))
+# """
+# Method 7: KNN
+# """
+# pipe_knn = Pipeline([('scl', StandardScaler()), ('knn', KNeighborsClassifier(n_neighbors=1))])
+# knn_score = []
+# fold_times = 1
+# for train_index, test_index in K_fold.split(X, y):
+#     pipe_knn.fit(X[train_index], y[train_index])
+#     score = pipe_knn.score(X[test_index], y[test_index])
+#     fold_times += 1
+#     knn_score.append(score)
+# knn_score_cv = [np.mean(knn_score), np.std(knn_score)]
+# print('KNN CV accuracy: %s +/- %s' % (knn_score_cv[0], knn_score_cv[1]))
 
 
 # np.savetxt('ppn.txt', np.array(ppn_error_sample))
